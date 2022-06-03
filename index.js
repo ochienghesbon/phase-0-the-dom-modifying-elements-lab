f45a478daaa4a2aa6acfb0e9383a229b27cff969
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-06-03 07:22:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// const element=document.getElementsByClassId("main");
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
